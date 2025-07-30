7c614ab121a4fcef8aad7f625c52b1f976968d55
Peixuan(Nancy) Xu <188550940+pxx88@users.noreply.github.com>
2025-07-30 15:24:33 -0500

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,17 +5,17 @@ var addressPoints = [
     -0.14405508452768728
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Master of Science in Psychology: Data Science in Human Behavior<br />Madison, WI, USA",
+    43.073051,
+    -89.40123
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Bacholar of Arts in Psychology<br />San Diego, CA, USA",
+    32.715736,
+    -117.161087
   ],
   [
-    "Associate degree in college<br />Walnut, CA, USA",
+    "Associate of Arts in Psychology<br />Walnut, CA, USA",
     34.02029,
     -117.86534
   ]
